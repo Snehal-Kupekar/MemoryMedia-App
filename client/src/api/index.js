a20944063f17ca71de3913c24e651e8de7435dd8
Snehal-Kupekar <71844562+Snehal-Kupekar@users.noreply.github.com>
2023-02-07 21:01:24 +0530

    Update server clinet

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+
 const url = 'http://localhost:5000/posts';
+const userUrl = 'http://localhost:5000/users'
 
 export const fetchPosts = () => {
     return (axios.get(url)
@@ -19,3 +21,16 @@ export const createPost = (newPost) => {
 export const updatePost = (id, updatedPost) => axios.put(`${url}/${id}`, updatedPost);
 
 export const deletePost = (id, post) => axios.delete(`${url}/${id}`, post);
+
+
+
+// user connection 
+ export const fetchUser = () =>{
+    return (axios.get(userUrl)
+    .catch(e => console.log(e)))
+ };
+
+ export const createUser = () =>{
+    return (axios.post(userUrl))
+    .catch(e=> console.log(e));
+ }
