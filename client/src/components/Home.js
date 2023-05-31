7104a7883fb4b2e3024084ef6873130741b4bbc4
Amark19 <amarkhamkar6@gmail.com>
2023-05-31 18:33:50 +0530

    Bugs fixes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,12 +10,12 @@ import {
   Grid,
   Button,
   Box,
-  
-
+  Menu,
+  MenuItem,
 } from "@material-ui/core";
 
 
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 
 
 import { getPosts } from "../actions/posts.js";
@@ -31,21 +31,30 @@ import useStyle from "../styles.js";
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
+
+  const [anchorEl, setAnchorEl] = useState(null);
   const classes = useStyle();
   const dispatch = useDispatch();
 
-  const [user ,setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
   const navigate = useNavigate();
 
   const currUser = JSON.parse(localStorage.getItem('profile'));
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-  
-  
-  console.log("after login who is user =>",currUser.result._id);
 
-  const logout = () =>{
-    dispatch({type : "LOGOUT"});
+
+  console.log("after login who is user =>", currUser.result._id);
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
     navigate('/')
     setUser(null);
   }
@@ -60,40 +69,62 @@ const Home = () => {
     dispatch(getPosts(currentUserId));
   }, [dispatch]);
 
-  
 
 
 
-  
+
+
   return (
-    
+
     <Box sx={{ flexGrow: 1 }}>
       <AppBar className={classes.appBar} position="static" color="inherit">
-          <Grid style={{marginTop:0}} container spacing={1}>
+        <Grid style={{ marginTop: 0 }} container spacing={1}>
           <Grid item xs={0} md={2}></Grid>
           <Grid item xs={6} md={6}>
-          <div style={{display:"flex"}}>
-          <Typography className={classes.heading} variant="h2" align="center">
-            Memories
-          </Typography>
-          <img
-            className={classes.image}
-            src={memories}
-            alt="memories"
-            height="60"
-          />
-          </div>
-      
-          </Grid>
-            
-          
-
-          <Grid item xs={6} md={4}  style={{display:"flex"}}>
-
-          <Button color="secondary" className={classes.button} onClick={logout}>LOGOUT</Button>
-          </Grid>
+            <div style={{ display: "flex" }}>
+              <Typography className={classes.heading} variant="h2" align="center">
+                Memories
+              </Typography>
+              <img
+                className={classes.image}
+                src={memories}
+                alt="memories"
+                height="60"
+              />
+            </div>
 
           </Grid>
+
+
+
+          <Grid item xs={6} md={4} style={{ display: "flex" }}>
+
+            <Button
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              Dashboard
+            </Button>
+
+
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>Hello, <span style={{ color: '#00b7ff' }}> {currentUser} </span></MenuItem>
+              <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+          </Grid>
+
+        </Grid>
       </AppBar>
       <Grow in>
         <Container>
@@ -104,7 +135,7 @@ const Home = () => {
             spacing={3}
           >
             <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
+              <Posts setCurrentId={setCurrentId} currentId={currentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
@@ -114,9 +145,14 @@ const Home = () => {
       </Grow>
     </Box>
 
-    
-    
+
+
   );
 };
+
+// const MenuItems = () => {
+//   return (
+//   );
+// }
 
 export default Home;
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, CircularProgress, Typography } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
@@ -8,38 +8,54 @@ import useStyles from "./styles";
 // useSelector is for fetching the posts stored in store
 const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
+  const [isFetched, setIsFetched] = useState(false);
+  const [postData, setPostData] = useState(null); // for storing the posts from the store
   const classes = useStyles();
 
-  console.log("post length", posts.length);
+  console.log("posts-outer", posts);
+  useEffect(() => {
+    if (postData != null) {
+      setIsFetched(true);
+    }
+    setPostData(posts.length);
+  }, [posts]);
 
-  return !posts.length ? (
-    <Typography
-      style={{
-        marginTop: "60px",
-        fontWeight: 400,
-        fontSize: "30px",
-        color:'#025464'
-        // lineHeight:'82px'
-      }}
-    >
-      Unleash your creativity and turn this empty space into a stunning showcase
-      for your cherished{" "}
-      <strong style={{ color: "#E57C23", fontSize: "40px" }}>MemOries</strong>!
-    </Typography>
-  ) : (
-    <Grid
-      className={classes.container}
-      container
-      alignItems="stretch"
-      spacing={3}
-    >
-      {posts.map((post) => (
-        <Grid key={post._id} item xs={12} sm={6} md={6}>
-          <Post post={post} setCurrentId={setCurrentId} />
+  if (!isFetched) {
+    return <CircularProgress />;
+  } else {
+    if (posts.length === 0) {
+      return (
+        <Typography
+          style={{
+            marginTop: "60px",
+            fontWeight: 400,
+            fontSize: "30px",
+            color: '#025464'
+            // lineHeight:'82px'
+          }}
+        >
+          Unleash your creativity and turn this empty space into a stunning showcase
+          for your cherished{" "}
+          <strong style={{ color: "#E57C23", fontSize: "40px" }}>MemOries</strong>!
+        </Typography>
+      );
+    } else {
+      return (
+        <Grid
+          className={classes.container}
+          container
+          alignItems="stretch"
+          spacing={3}
+        >
+          {posts.map((post) => (
+            <Grid key={post._id} item xs={12} sm={6} md={6}>
+              <Post post={post} setCurrentId={setCurrentId} />
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+      );
+    }
+  }
 };
 
 export default Posts;
